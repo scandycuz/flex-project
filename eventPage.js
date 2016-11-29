54c28c13cf28e92e9a2feb006641350a35bc77aa
scandycuz <trevorscandalios@gmail.com>
2016-11-29 12:25:26 -0800

    Adds logic to only block requests when timer active

--- eventPage.js
+++ eventPage.js
@@ -5,71 +5,101 @@ let height = 300;
 
 let blockedDomains;
 let domainArray;
+let blockActive = false;
 
-function blockedListener() {
-  chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-    let currentUrl = tabs[0].url;
-    let currentTabId = tabs[0].id;
-    let currentUrlString = currentUrl.split(/\/\/|\//)[1];
-
-    for (let i = 0; i < blockedDomains.length; i++) {
-      let domain = blockedDomains[i].replace(/\s/g, '');
-      if (currentUrlString.includes(domain)) {
-        chrome.windows.create({
-          url: "dialog.html",
-          type: "panel",
-          focused: true,
-          width: width,
-          height: height,
-          left: Math.round((screenWidth-width)/2),
-          top: Math.round((screenHeight-height)/2)
-        });
-        chrome.tabs.update(currentTabId, {url: "http://www.theguardian.com"});
-        return;
+function filterInt() {
+  const _filterInt = setInterval(function() {
+    chrome.storage.sync.get({
+      timerEnd: 0
+    }, function(items) {
+      let endingTimestampSeconds = items.timerEnd;
+      let currentTimestampSeconds = Date.now() / 1000;
+      alert('interval');
+      if (endingTimestampSeconds - currentTimestampSeconds > 0) {
+        blockActive = true;
+      } else {
+        blockActive = false;
+        clearInterval(_filterInt);
       }
-    }
-  });
+    });
+  }, 60000);
 }
 
 function requestListener() {
-  return {cancel: true}
+  if (blockActive) {
+    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+      let currentUrl = tabs[0].url;
+      let currentTabId = tabs[0].id;
+      let currentUrlString = currentUrl.split(/\/\/|\//)[1];
+
+      for (let i = 0; i < blockedDomains.length; i++) {
+        let domain = blockedDomains[i].replace(/\s/g, '');
+        if (currentUrlString.includes(domain)) {
+          chrome.windows.create({
+            url: "dialog.html",
+            type: "panel",
+            focused: true,
+            width: width,
+            height: height,
+            left: Math.round((screenWidth-width)/2),
+            top: Math.round((screenHeight-height)/2)
+          });
+          chrome.tabs.update(currentTabId, {url: "http://www.theguardian.com"});
+          return;
+        }
+      }
+    });
+    return {cancel: true}
+  } else {
+    return {cancel: false}
+  }
 }
 
-chrome.storage.onChanged.addListener(function() {
-  chrome.storage.sync.get({
-    blockedSites: ["facebook.com", "twitter.com"]
-    }, function(items) {
-      blockedDomains = items.blockedSites;
-      domainArray = blockedDomains.map ( (domain) => (
-        `*://*.${domain}/*`
-      ));
-
-    chrome.webRequest.onBeforeRequest.removeListener(requestListener);
-    chrome.webRequest.onBeforeRequest.removeListener(blockedListener);
-    chrome.webRequest.onBeforeRequest.addListener(requestListener, {
-      urls: domainArray
-    }, ["blocking"]);
-    chrome.webRequest.onErrorOccurred.addListener(blockedListener, {
-      urls: domainArray
-    });
-  });
-});
-
 chrome.storage.sync.get({
-  blockedSites: ["facebook.com", "twitter.com"]
+    blockedSites: ["facebook.com", "twitter.com"],
+    timerEnd: 0
   }, function(items) {
     blockedDomains = items.blockedSites;
+    let endingTimestampSeconds = items.timerEnd;
+    let currentTimestampSeconds = Date.now() / 1000;
+    if (endingTimestampSeconds - currentTimestampSeconds > 0) {
+      blockActive = true;
+      filterInt();
+    } else {
+      blockActive = false;
+    }
     domainArray = blockedDomains.map ( (domain) => (
       `*://*.${domain}/*`
     )
   );
 
   chrome.webRequest.onBeforeRequest.removeListener(requestListener);
-  chrome.webRequest.onBeforeRequest.removeListener(blockedListener);
   chrome.webRequest.onBeforeRequest.addListener(requestListener, {
     urls: domainArray
   }, ["blocking"]);
-  chrome.webRequest.onErrorOccurred.addListener(blockedListener, {
-    urls: domainArray
+});
+
+chrome.storage.onChanged.addListener(function() {
+  chrome.storage.sync.get({
+      blockedSites: ["facebook.com", "twitter.com"],
+      timerEnd: 0
+    }, function(items) {
+      blockedDomains = items.blockedSites;
+      let endingTimestampSeconds = items.timerEnd;
+      let currentTimestampSeconds = Date.now() / 1000;
+      if (endingTimestampSeconds - currentTimestampSeconds > 0) {
+        blockActive = true;
+        filterInt();
+      } else {
+        blockActive = false;
+      }
+      domainArray = blockedDomains.map ( (domain) => (
+        `*://*.${domain}/*`
+      ));
+
+    chrome.webRequest.onBeforeRequest.removeListener(requestListener);
+    chrome.webRequest.onBeforeRequest.addListener(requestListener, {
+      urls: domainArray
+    }, ["blocking"]);
   });
 });
